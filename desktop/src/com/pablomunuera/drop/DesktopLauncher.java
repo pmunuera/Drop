a014faf3c72c5920a467a0ae744a121b6b44bbb0
pmunuera <95918506+pmunuera@users.noreply.github.com>
2023-03-27 17:11:18 +0200

    Initial commit

---
+++ desktop/src/com/pablomunuera/drop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.pablomunuera.drop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.pablomunuera.drop.Drop;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Drop");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
+		config.setForegroundFPS(60);
+		new Lwjgl3Application(new Drop(), config);
+	}
+}
